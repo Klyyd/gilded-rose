db4cf14632897a11e856830f5324a8f9779466ae
Louish80 <huret.louispro@hotmail.com>
2021-11-30 13:47:41 +0100

    sart test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,7 +3,12 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
+    @Test
+    void should_build() {
+        assertTrue(true);
+    }
 }
